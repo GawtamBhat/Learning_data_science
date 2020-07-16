b674123a8ea3c4e44039c1ae4ff8ba7cd46a1eaa
Gawtam <53069725+infinitebhat@users.noreply.github.com>
2020-07-16 16:02:26 -0400

    Update Guided Image classifiction Tensorflow project in R.R

--- Guided Image classifiction Tensorflow project in R.R
+++ Guided Image classifiction Tensorflow project in R.R
@@ -2,14 +2,11 @@
 ## THIS ALSO USES TENSOR FLOW FOR MACHINE LEARNING.
 
 print("Shree Ganeshaya Namaha")
-install.packages("tensorflow")
-library(tensorflow)
-install_tensorflow()
-tensorflow::install_tensorflow()
-
 library(EBImage)
 library(keras)
+library(tensorflow)
 
+setwd('/Users/gawtambhat/Documents/R Studio/Projects/Image Recognition and Classification')
 pictures <- c('b1.jpg', 'b2.jpg', 'b3.jpg','b4.jpg','b5.jpg','b6.jpg',
               'c1.jpg', 'c2.jpg', 'c3.jpg', 'c4.jpg', 'c5.jpg', 'c6.jpg')
 
@@ -37,6 +34,7 @@ str(mypictures)
 
 #Row Bind
 trainX <- NULL
+for (i in 1:5) {trainX <- rbind(trainX, mypictures[[i]])}
 for (i in 7:11 ) {trainX <- rbind(trainX, mypictures[[i]])}
 str(trainX)
 
@@ -48,22 +46,45 @@ testY<- c(0,1)
 
 #One hot encoding
 trainLabels <- to_categorical(trainY)
-tesLabels <- to_categorical(testY)
+testLabels <- to_categorical(testY)
+
+trainLabels
+
+# Model
+model <- keras_model_sequential()
+model %>%
+  layer_dense(units=256, activation='relu', input_shape = c(2352)) %>%
+  layer_dense(unit=128, activation= 'relu')%>%
+  layer_dense(units=2, activation = 'softmax')
+summary(model)
+
+#Compile
+model%>%
+  compile(loss='binary_crossentropy',
+          optimizer= optimizer_rmsprop(),
+          metrics=c('accuracy'))
+
+#Fit model
+history <- model %>%
+  fit(trainX, 
+      trainLabels, 
+      epochs= 30, 
+      batch_size=32, 
+      validation_split= 0.2)
+
+plot(history)
 
 
-/Users/gawtambhat/Documents/R Studio/Projects/Image Recognition and Classification
+#Evaluation & Prediction - tRAIN data
+model %>% evaluate(trainX, trainLabels)
+pred <- model %>% predict_classes(trainX)
+table(Predicted= pred, Actual= trainY)
 
+prob <- model %>% predict_proba(trainX)
+cbind(prob, Prected= pred, Actual= trainY)
 
-
-
-
-
-
-
-
-
-
-
-
-
+#Evaluation & Prediction - test data
+model %>% evaluate(testX, testLabels)
+pred <- model %>% predict_classes((testX))
+table(Prected= pred, Actual= testY)
 
